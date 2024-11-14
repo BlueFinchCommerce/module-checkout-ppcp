a02714881c1c7e23d4473d0e59583e3892eb6f4e
Chloe <chloe@gene.co.uk>
2024-11-14 15:57:43 +0000

    feature/setup-base-module

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Gene_BetterCheckoutNewModule',
+    'Gene_BetterCheckoutPPCP',
     __DIR__
 );
