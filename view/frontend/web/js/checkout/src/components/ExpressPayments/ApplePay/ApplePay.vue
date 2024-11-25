16b38d1f9206f67c7f77c6d93395a9a7a4638ec0
Alla Nehaenko <allanehaenko@gmail.com>
2024-11-25 12:27:15 +0000

    added templates for each express payment methods

---
+++ view/frontend/web/js/checkout/src/components/ExpressPayments/ApplePay/ApplePay.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Express PpcpApplePay</p>
+</template>
+
+<script>
+export default {
+  name: 'PpcpApplePay',
+  mounted() {
+    console.log('PpcpApplePay')
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../expressPayments.scss";
+</style>
---
+++ view/frontend/web/js/checkout/src/components/ExpressPayments/GooglePay/GooglePay.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Express googlepay</p>
+</template>
+
+<script>
+export default {
+  name: 'PpcpGooglePay',
+  mounted() {
+    console.log('google pay')
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../expressPayments.scss";
+</style>
