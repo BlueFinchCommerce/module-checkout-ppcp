c3820e16f70ef3b9f23ced15f00c11a84f256922
Alla Nehaenko <allanehaenko@gmail.com>
2024-12-12 15:07:11 +0000

    added fix for lint errors in clear session functionality

--- view/frontend/web/js/cart-clear.js
+++ view/frontend/web/js/cart-clear.js
@@ -1,10 +1,11 @@
- /**
+/**
  * Added cart invalidation to clear the mini cart.
  */
- define(['Magento_Customer/js/customer-data'], function (customerData) {
-   'use strict';
+define(['Magento_Customer/js/customer-data'], function (customerData) {
+    'use strict';
     return function () {
         const sections = ['cart'];
+
         customerData.invalidate(sections);
         customerData.reload(sections, true);
     };
